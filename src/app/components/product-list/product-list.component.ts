38f28afd68e391c063c8cd3d749c5c5d6417a42b
Jed <thanhtrung1012000@gmail.com>
2023-06-20 16:54:35 +0700

    first commit

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from "../../services/product.service";
+import { Product } from "../../common/product";
+
+@Component({
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.scss"],
+})
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.listProducts();
+  }
+
+  listProducts() {
+    this.productService.getProductList().subscribe((data) => {
+      this.products = data;
+    });
+  }
+}
