bb12bd59c848bd8683afcf5c821147dd461da883
Jed <thanhtrung1012000@gmail.com>
2023-06-30 01:38:20 +0700

    feat: add filter products feature by category

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,26 +1,38 @@
 import { Component, OnInit } from "@angular/core";
 import { ProductService } from "../../services/product.service";
 import { Product } from "../../common/product";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-product-list",
-  // templateUrl: "./product-list.component.html",
-  // templateUrl: "./product-list-table.component.html",
   templateUrl: "./product-list-grid.component.html",
   styleUrls: ["./product-list.component.scss"],
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
+  currentCategoryId: number = 1;
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe((data) => {
-      this.products = data;
-    });
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has("id");
+    this.currentCategoryId = hasCategoryId
+      ? Number(this.route.snapshot.paramMap.get("id"))
+      : 1;
+
+    this.productService
+      .getProductList(this.currentCategoryId)
+      .subscribe((data) => {
+        this.products = data;
+      });
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,24 +2,41 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { map, Observable } from "rxjs";
 import { Product } from "../common/product";
+import { ProductCategory } from "../common/product-category";
 
 @Injectable({
   providedIn: "root",
 })
 export class ProductService {
-  private baseUrl = "http://localhost:8080/api/products";
+  private baseUrl = "http://localhost:8080/api";
 
   constructor(private httpClient: HttpClient) {}
 
-  getProductList(): Observable<Product[]> {
+  getProductList(categoryId: number): Observable<Product[]> {
+    const searchUrl = `${this.baseUrl}/products/search/findByCategoryId?id=${categoryId}`;
+
     return this.httpClient
-      .get<GetResponse>(this.baseUrl)
+      .get<GetResponseProducts>(searchUrl)
       .pipe(map((response) => response._embedded.products));
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    const categoryUrl = `${this.baseUrl}/product-category`;
+
+    return this.httpClient
+      .get<GetResponseProductCategory>(categoryUrl)
+      .pipe(map((response) => response._embedded.productCategory));
   }
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  };
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   };
 }
